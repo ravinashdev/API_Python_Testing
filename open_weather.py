7d6cfedee09809ec488877f148edcc45734b9a04
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-13 00:13:07 -0400

    Twilio API send SMS

--- open_weather.py
+++ open_weather.py
@@ -1,16 +1,22 @@
 # ---------------------------- IMPORTS ------------------------------- #
 # Allows you to read the .env file
+from pyexpat.errors import messages
+
 from dotenv import load_dotenv
 import os
+# Twillio
+from twilio.rest import Client
 # Async API Python calls
 import asyncio
 import httpx
-import datetime as dt
 # ---------------------------- CONSTANTS ------------------------------- #
 load_dotenv()
 IP_LOCATION_API_ENDPOINT= os.getenv("IP_LOCATION_API_ENDPOINT")
 OPEN_WEATHER_API_KEY= os.getenv("OPEN_WEATHER_API_KEY")
 OPEN_WEATHER_API_ENDPOINT= os.getenv("OPEN_WEATHER_API_ENDPOINT")
+TWILLIO_TEST_ACCOUNT_SID= os.getenv("TWILLIO_TEST_ACCOUNT_SID")
+TWILLIO_TEST_AUTH_TOKEN= os.getenv("TWILLIO_TEST_AUTH_TOKEN")
+
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 
 
@@ -40,4 +46,14 @@ async def main():
 
 # ---------------------------- UI SETUP ------------------------------- #
 current_weather_data = asyncio.run(main())
-print(current_weather_data)
+current_city = current_weather_data[0]["name"]
+current_description = current_weather_data[0]["weather"][0]["description"]
+message=f"Weather Update: There are some {current_description} in {current_city} right now"
+
+# Twillio SMS Test
+client = Client(TWILLIO_TEST_ACCOUNT_SID, TWILLIO_TEST_AUTH_TOKEN)
+message = client.messages.create(
+    body=message,
+    from_="+18559411028",
+    to="+13478579787",
+)
